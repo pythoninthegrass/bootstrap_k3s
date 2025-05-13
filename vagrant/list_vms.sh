637f81a20b0a09d70aad05841e67176d2b831072
pythoninthegrass <4097471+pythoninthegrass@users.noreply.github.com>
2025-05-13 13:36:31 -0500

    refactor: vm output as table

--- vagrant/list_vms.sh
+++ vagrant/list_vms.sh
@@ -1,8 +1,31 @@
 #!/usr/bin/env bash
 
-echo "VM Name: IP Address"
+# Get all running VMs
+vms=$(vagrant status | grep running | grep libvirt | awk '{print $1}')
 
-(for vm in $(vagrant status | grep running | grep libvirt | awk '{print $1}'); do
-  domain_name=$(virsh list --all | grep "$vm" | awk '{print $2}')
-  printf "%-16s %s\n" "$vm:" "$(virsh domifaddr "$domain_name" | grep ipv4 | awk '{print $4}' | cut -d'/' -f1)"
+# Find the length of the longest VM name and add padding
+vm_col_width=8
+for vm in $vms; do
+	if [ ${#vm} -gt "$vm_col_width" ]; then
+		vm_col_width=${#vm}
+	fi
+done
+vm_col_width=$((vm_col_width + 2)) # Add padding
+
+# Set IP column width
+ip_col_width=16
+
+# Print table with dynamic width
+border=$(printf "%0.s-" $(seq 1 $((vm_col_width + 2))))
+ip_border=$(printf "%0.s-" $(seq 1 $((ip_col_width + 2))))
+printf "+%s+%s+\n" "$border" "$ip_border"
+printf "| %-${vm_col_width}s | %-${ip_col_width}s |\n" "VM Name" "IP Address"
+printf "+%s+%s+\n" "$border" "$ip_border"
+
+(for vm in $vms; do
+	domain_name=$(virsh list --all | grep "$vm" | awk '{print $2}')
+	ip_addr=$(virsh domifaddr "$domain_name" | grep ipv4 | awk '{print $4}' | cut -d'/' -f1)
+	printf "| %-${vm_col_width}s | %-${ip_col_width}s |\n" "$vm" "$ip_addr"
 done) 2>&1 | awk '!/\[fog\]\[WARNING\]/'
+
+printf "+%s+%s+\n" "$border" "$ip_border"
