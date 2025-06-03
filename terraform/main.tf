e7b6ffa7c67128e504564ebe2ee39bd162cf9805
pythoninthegrass <4097471+pythoninthegrass@users.noreply.github.com>
2025-06-03 13:29:09 +0000

    chore: split providers into separate file

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,18 +1,3 @@
-terraform {
-  required_version = ">= 1.0"
-  required_providers {
-    libvirt = {
-      source  = "dmacvicar/libvirt"
-      version = "~> 0.7.6"
-    }
-  }
-}
-
-provider "libvirt" {
-  uri = var.libvirt_uri
-}
-
-# Ubuntu 24.04 base image
 resource "libvirt_volume" "ubuntu_base" {
   name   = "ubuntu-24.04-base.qcow2"
   source = "https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-amd64.img"
---
+++ terraform/providers.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    libvirt = {
+      source  = "dmacvicar/libvirt"
+      version = "~> 0.8.3"
+    }
+  }
+}
+
+provider "libvirt" {
+  uri = var.libvirt_uri
+}
