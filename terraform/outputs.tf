7b6d9aa312f8f58001f6decaacc5d2aa455f7300
pythoninthegrass <4097471+pythoninthegrass@users.noreply.github.com>
2025-06-03 15:00:16 -0500

    chore (terraform): additional outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -24,3 +24,64 @@ output "ssh_config" {
     nodes = local.all_nodes
   })
 }
+
+output "control_plane_count" {
+  description = "Number of control plane nodes"
+  value       = var.control_plane_count
+}
+
+output "worker_count" {
+  description = "Number of worker nodes"
+  value       = var.worker_count
+}
+
+output "memory_control" {
+  description = "Memory allocation for control plane nodes (MB)"
+  value       = var.memory_control
+}
+
+output "memory_worker" {
+  description = "Memory allocation for worker nodes (MB)"
+  value       = var.memory_worker
+}
+
+output "cpu_control" {
+  description = "CPU cores for control plane nodes"
+  value       = var.cpu_control
+}
+
+output "cpu_worker" {
+  description = "CPU cores for worker nodes"
+  value       = var.cpu_worker
+}
+
+output "disk_size" {
+  description = "Disk size for all nodes (GB)"
+  value       = var.disk_size
+}
+
+output "base_ip" {
+  description = "Base IP address for the network"
+  value       = var.base_ip
+}
+
+output "network_name" {
+  description = "Name of the libvirt network"
+  value       = var.network_name
+}
+
+output "use_bridge_network" {
+  description = "Whether bridge networking is enabled"
+  value       = var.use_bridge_network
+}
+
+output "ssh_public_key" {
+  description = "SSH public key for VM access"
+  value       = var.ssh_public_key
+  sensitive   = true
+}
+
+output "github_user" {
+  description = "GitHub username for SSH key import"
+  value       = var.github_user
+}
